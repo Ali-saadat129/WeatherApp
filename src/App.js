0a1fc1245376f28b735208f435b153399b4e3986
Ali Saadat <alisaadt129@gmail.com>
2023-04-07 21:50:34 +0330

    continue the priject

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-
 // Api
 import {Getapi} from './Api/Api.js'
 // Hooks
 import { useState,useEffect } from 'react';
 import axios from 'axios';
+// css
+import Styles from "./styles/App.module.css"
+// bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+// icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGrip } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 
 function App() {
 
@@ -12,16 +21,25 @@ function App() {
 
   useEffect( () => {
 
-    Getapi(setdata,"Tehran")
+    // Getapi(setdata,"Tehran")
 
   },[])
   
 
-console.log(data?.location?.name)
+console.log(data)
 
   return (
-    <div className="App">
-      <p>{data?.location?.name}</p>
+    <div className={`  ${Styles.App}`}>
+      <div className={Styles.headermeno}> 
+        <h1><FontAwesomeIcon icon={faGrip} /></h1>
+      </div> 
+
+      <div className={`${Styles.searchbar}`}>
+          <div className={`${Styles.inputparentt}`}> 
+            <input type="text" className={`form-control`} placeholder="Search your town " ></input>
+          </div>
+      </div>
+
     </div>
   );
 }
