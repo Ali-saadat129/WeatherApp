ec5246e1599c70e8583b5253ff8e45d00e141bd0
Ali saadat <alisaadt129@gmail.com>
2023-04-13 17:58:27 +0330

    problem resive data solved and used in footer

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,9 +3,10 @@ import axios from "axios"
 
 
 const Getapi = async (setdata,City) => {
-    const Url = `https://api.weatherapi.com/v1/current.json?key=ded7eb6daea94485bc2130029232903&q=Tehran`
+    const Url = `https://api.weatherapi.com/v1/current.json?key=ded7eb6daea94485bc2130029232903&q=${City}`
     // const Url = "https://jsonplaceholder.typicode.com/todos/1"
     const response = await axios.get(Url)
+    // console.log(response.data)
     setdata(response.data)
     // return response.data
 };
--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import axios from 'axios';
 import Styles from "./styles/App.module.css"
 // bootstrap 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// icons
+// icons and picture
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGrip } from '@fortawesome/free-solid-svg-icons'
-
+import suncloudy from './icons/cludy_sun.png'
 
 
 
@@ -18,15 +18,21 @@ import { faGrip } from '@fortawesome/free-solid-svg-icons'
 function App() {
 
   const [data,setdata] = useState([])
+  const [city1,setcity1] = useState([])
+  const [city2,setcity2] = useState([])
+  const [city3,setcity3] = useState([])
+
+  const datalist = []
 
   useEffect( () => {
-
-    // Getapi(setdata,"Tehran")
+    
+    Getapi(setdata,"Tehran")
+    Getapi(setcity1,"Tehran")
+    Getapi(setcity2,"London")
+    Getapi(setcity3,"New-york")
 
   },[])
   
-
-console.log(data)
 
   return (
     <div className={`  ${Styles.App}`}>
@@ -38,6 +44,45 @@ console.log(data)
           <div className={`${Styles.inputparentt}`}> 
             <input type="text" className={`form-control`} placeholder="Search your town " ></input>
           </div>
+      </div>
+
+      <div className={`container-fluid ${Styles.middle_part}`}>
+        <div className={` row ${Styles.middle_row}`}>
+
+          <div className={`col-6`}> 
+            <h1>10</h1>
+          </div>
+
+          <div className={`col-6`}>
+            <h2>{data?.location?.name}</h2>
+            <h2><img alt='weathericons' src={suncloudy}></img></h2>
+          </div>
+
+        </div>
+      </div>
+      
+      <div className={` container-fluid ${Styles.footer}`} >
+        <div className={` row ${Styles.footer_parent}`}>
+
+      
+          <div className={`col-4 ${Styles.globalpart}`}>
+           <h3>{city1?.location?.country} /{city1?.location?.name}</h3>
+            <p>Time : {city1?.location?.localtime}</p>
+            <p> Temp : {city1?.current?.temp_c} </p>
+          </div>
+
+          <div className={`col-4 ${Styles.globalpart}`}>
+           <h3>{city2?.location?.country} /{city2?.location?.name}</h3>
+            <p>Time : {city2?.location?.localtime}</p>
+            <p> Temp : {city2?.current?.temp_c} </p>
+          </div>
+
+          <div className={`col-4 ${Styles.globalpart}`}>
+           <h3>{city3?.location?.country} /{city3?.location?.name}</h3>
+            <p>Time : {city3?.location?.localtime}</p>
+            <p> Temp : {city3?.current?.temp_c} </p>
+          </div>
+        </div>
       </div>
 
     </div>
